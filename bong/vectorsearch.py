65beea2fdfd221ee289b11835cd438d13a3491ed
Carl Bordum Hansen <carl@bordum.dk>
2017-06-25 18:22:32 +0200

    made functions accept documents

--- bong/vectorsearch.py
+++ bong/vectorsearch.py
@@ -1,13 +1,14 @@
 """
-    bong.py
-    ~~~~~~~
+    vectorsearch.py
+    ~~~~~~~~~~~~~~~
 
-    Search engine in Python.
+    Implements the functionality to compare documents as if they were
+    vectors. More specifically it implements the vector space model.
 """
 
 
 __all__ = [
-    'DocumentVector',
+    'magnitude',
     'dot_product',
     'cosine_similarity',
     'vector_space_search',
@@ -16,53 +17,43 @@ __author__ = 'Carl Bordum Hansen'
 __license__ = 'MIT'
 
 
-from collections import Counter
 from math import sqrt
 
 
-class DocumentVector(Counter):
-    """Represent a document as a vector.
+def magnitude(document):
+    """Return the magnitude of *document*.
 
-    Subclass of :class:'collections.Counter'.
+    https://en.wikipedia.org/wiki/Magnitude_(mathematics)
+
+    :rtype: float.
     """
-
-    def __init__(self, document):
-        super().__init__(tokenize(document))
-        self.document = document
-
-    @property
-    def magnitude(self):
-        """Return the magnitude of vector.
-
-        https://en.wikipedia.org/wiki/Magnitude_(mathematics)
-
-        :rtype: float.
-        """
-        return sqrt(sum(i**2 for i in self.values()))
+    return sqrt(sum(i**2 for i in document.token_counter.values()))
 
 
-def dot_product(v1, v2):
-    """Return the dot product of two vectors.
+def dot_product(doc1, doc2):
+    """Return the dot product of two documents.
 
     https://en.wikipedia.org/wiki/Dot_product#Algebraic_definition
 
     :rtype: int.
     """
+    v1 = doc1.token_counter
+    v2 = doc2.token_counter
     return sum(count*v2[term] for term, count in v1.items())
 
 
-def cosine_similarity(v1, v2):
-    """Return the cosine similarity of two vectors.
+def cosine_similarity(doc1, doc2):
+    """Return the cosine similarity of two documents.
 
     https://en.wikipedia.org/wiki/Cosine_similarity
 
     :rtype: float.
     """
-    return dot_product(v1, v2) / (v1.magnitude * v2.magnitude)
+    return dot_product(doc1, doc2) / (magnitude(doc1) * magnitude(doc2))
 
 
 def vector_space_search(query, *documents):
-    """Return (vector of document, score) for *documents*.
+    """Return (document, score) for each document in *documents*.
 
     The score is the cosine similarity between the query and the document.
     The closer score is to 1, the better the result is. 0 means zero
@@ -70,19 +61,9 @@ def vector_space_search(query, *documents):
 
     http://ondoc.logand.com/d/2697/pdf
 
-    :param query: str.
-    :param documents: iterable of strs.
-    :rtype: generator, yields (vector, cosign)
+    :param query: document.
+    :param documents: iterable of documents.
+    :rtype: generator, yields (doc, similarity)
     """
-    vectors = [DocumentVector(document) for document in documents]
-    query_vector = DocumentVector(query)
-    for vector in vectors:
-        yield vector, cosine_similarity(query_vector, vector)
-
-
-if __name__ == '__main__':
-    d1 = 'cat mouse mouse cat dog mouse cat mouse'
-    d2 = 'dog cat dog mouse mouse mouse mouse mouse'
-    d3 = 'dog cat dog cat dog'
-    for vector, result in vector_space_search('mouse', d1, d2, d3):
-        print(vector, ':', result)
+    for doc in documents:
+        yield doc, cosine_similarity(query, doc)
