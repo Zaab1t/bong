31678251b34eaa750168487fe1b3ea226e4af28c
Carl Bordum Hansen <carl@bordum.dk>
2017-06-27 16:32:30 +0200

    first version of search()

--- bong/bong.py
+++ bong/bong.py
@@ -11,9 +11,21 @@ __author__ = 'Carl Bordum Hansen'
 __license__ = 'MIT'
 
 
+from pymongo import MongoClient
+from itertools import islice
+
+from .vectorsearch import vector_space_search
+
+
 def search(query, n):
     """Search for *query* and return the *n* most relevant results.
 
     It is important that *query* is a subclass of
     :class:'bong.BaseDocument'.
     """
+    client = MongoClient()
+    db = client['bong']
+    collection = db['documents']
+    result = vector_space_search(query, collection.find())
+    for doc, similarity in islice(reversed(sorted(result)), n):
+        yield doc
