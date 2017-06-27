3ce1fa892c4c5088d457ce2768ebdebd6ba79cff
Carl Bordum Hansen <carl@bordum.dk>
2017-06-27 17:52:49 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='bong',
+    description='Search Engine written in Python',
+    url='https://github.com/Zaab1t/bong',
+    author='Carl Bordum Hansen',
+    license='MIT',
+    packages=['bong'],
+)
